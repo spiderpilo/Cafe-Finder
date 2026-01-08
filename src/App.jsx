2bb25ea57115ae418eb4ac9776aaf39d495f2ef6
spiderpilo <piolo.patag@gmail.com>
2026-01-07 20:06:18 -0800

    imported coffee icon, updated home page with assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
 import "./App.css";
-export default function App() {
-  return(
-    <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<div>Home</div>} />
-     </Routes>
-    </BrowserRouter>
-  )
-}
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
-export default function Home(){
-    return(
-    <div>
-        <h1>Cafe Finder</h1>
-        <p>home page</p>
+import cafeIcon from "../assets/CoffeeFinder_Icon.png";
+
+export default function Home() {
+  return (
+    <div className="panel">
+      <h1 className="home-title">Cafe Finder</h1>
+
+      <img
+        src={cafeIcon}
+        alt="Cafe Finder Icon"
+        className="home-icon"
+      />
     </div>
-    );
+  );
 }
